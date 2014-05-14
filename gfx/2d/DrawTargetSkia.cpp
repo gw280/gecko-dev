a9e39114c4071f4c690e6b852913c912ab75f51f
George Wright <george@mozilla.com>
2014-05-14 00:08:04 -0400

    Bug 1008267 - Stop using get/setLocalMatrix and use a LocalMatrixShader instead in DrawTargetSkia r=snorp

--- gfx/2d/DrawTargetSkia.cpp
+++ gfx/2d/DrawTargetSkia.cpp
@@ -167,8 +167,8 @@ SetPaintPattern(SkPaint& aPaint, const Pattern& aPattern, TempBitmap& aTmpBitmap
         if (shader) {
             SkMatrix mat;
             GfxMatrixToSkiaMatrix(pat.mMatrix, mat);
-            shader->setLocalMatrix(mat);
-            SkSafeUnref(aPaint.setShader(shader));
+            SkShader* transformedShader = SkShader::CreateLocalMatrixShader(shader, mat);
+            SkSafeUnref(aPaint.setShader(transformedShader));
         }
 
       } else {
@@ -197,8 +197,8 @@ SetPaintPattern(SkPaint& aPaint, const Pattern& aPattern, TempBitmap& aTmpBitmap
         if (shader) {
             SkMatrix mat;
             GfxMatrixToSkiaMatrix(pat.mMatrix, mat);
-            shader->setLocalMatrix(mat);
-            SkSafeUnref(aPaint.setShader(shader));
+            SkShader* transformedShader = SkShader::CreateLocalMatrixShader(shader, mat);
+            SkSafeUnref(aPaint.setShader(transformedShader));
         }
 
       } else {
@@ -215,8 +215,8 @@ SetPaintPattern(SkPaint& aPaint, const Pattern& aPattern, TempBitmap& aTmpBitmap
       SkShader* shader = SkShader::CreateBitmapShader(bitmap, mode, mode);
       SkMatrix mat;
       GfxMatrixToSkiaMatrix(pat.mMatrix, mat);
-      shader->setLocalMatrix(mat);
-      SkSafeUnref(aPaint.setShader(shader));
+      SkShader* transformedShader = SkShader::CreateLocalMatrixShader(shader, mat);
+      SkSafeUnref(aPaint.setShader(transformedShader));
       if (pat.mFilter == Filter::POINT) {
         aPaint.setFilterLevel(SkPaint::kNone_FilterLevel);
       }
@@ -558,7 +558,8 @@ DrawTargetSkia::MaskSurface(const Pattern &aSource,
 
   SkMatrix transform = maskPaint.getShader()->getLocalMatrix();
   transform.postTranslate(SkFloatToScalar(aOffset.x), SkFloatToScalar(aOffset.y));
-  maskPaint.getShader()->setLocalMatrix(transform);
+  SkShader* transformedShader = SkShader::CreateLocalMatrixShader(maskPaint.getShader(), transform);
+  maskPaint.setShader(transformedShader);
 
   SkLayerRasterizer *raster = new SkLayerRasterizer();
   raster->addLayer(maskPaint);
